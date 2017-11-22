d307228c3e144af775e4a9e94529dd8cba989e17
txj <1101774689@qq.com>
2017-11-22 17:45:44 +0800

    first add

---
+++ src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <div id="app">
+    <m-header></m-header>
+    <tab></tab>
+    <keep-alive>
+      <router-view></router-view>
+    </keep-alive>
+  </div>
+</template>
+
+<script>
+  import MHeader from 'components/m-header/m-header.vue'
+  import Tab from 'components/tab/tab.vue'
+  export default {
+    name: 'app',
+    components :{
+      MHeader ,
+      Tab
+    }
+  }
+</script>
+
+<style>
+#app {
+
+}
+</style>
